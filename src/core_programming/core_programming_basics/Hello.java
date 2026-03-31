c5097dcf8632c73d2963a1ac594fb3b3da83ad84
Piyush Rathor <p.rathor2811@gmail.com>
2026-03-31 22:12:03 +0530

    [Piyush Rathor] Add . Variable practice

---
+++ src/core_programming/core_programming_basics/Hello.java
@@ -0,0 +1,5 @@
+package core_programming.core_programming_basics;
+
+public class Hello {
+
+}
---
+++ src/core_programming/core_programming_basics/VariablesDemo.java
@@ -0,0 +1,15 @@
+package core_programming.core_programming_basics;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 21;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean isActive = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Active: " + isActive);
+    }
+}
