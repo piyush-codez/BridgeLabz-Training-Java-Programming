a6b31cd85821ef5b43fd9f707183e673f113a9ac
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-04 10:58:33 +0530

    Checking Divisibility Of 5

---
+++ core-programming/core_programming_intermediate/Control_Flow/CheckDivisibilityOf5.java
@@ -0,0 +1,15 @@
+package core_programming_intermediate.Control_Flow;
+
+import java.util.Scanner;
+
+public class CheckDivisibilityOf5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%5 == 0){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+    }
+}
