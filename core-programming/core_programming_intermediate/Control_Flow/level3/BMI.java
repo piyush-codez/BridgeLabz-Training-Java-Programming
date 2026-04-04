560bed102b4656f6c11d0a251333aea6188a1642
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-05 00:57:44 +0530

    BMI finder

---
+++ core-programming/core_programming_intermediate/Control_Flow/level3/BMI.java
@@ -0,0 +1,30 @@
+package core_programming_intermediate.Control_Flow.level3;
+
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter weight in kg: ");
+        double weight = sc.nextDouble();
+
+        System.out.print("Enter height in cm: ");
+        double heightCm = sc.nextDouble();
+
+        double heightMeter = heightCm / 100;
+        double bmi = weight / (heightMeter * heightMeter);
+
+        System.out.println("BMI = " + bmi);
+
+        if (bmi <= 18.4) {
+            System.out.println("Underweight");
+        } else if (bmi <= 24.9) {
+            System.out.println("Normal");
+        } else if (bmi <= 39.9) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
+        }
+    }
+}
