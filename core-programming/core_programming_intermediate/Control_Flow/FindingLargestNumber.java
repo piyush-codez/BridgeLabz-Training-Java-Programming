a78b73294f723e86f99ab05b47c7ae67895e3bb8
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-04 11:05:23 +0530

    Finding the largest of all

---
+++ core-programming/core_programming_intermediate/Control_Flow/FindingLargestNumber.java
@@ -0,0 +1,17 @@
+package core_programming_intermediate.Control_Flow;
+
+import java.util.Scanner;
+
+public class FindingLargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        if(a > b && a > c){
+            System.out.println("First Number is Largest");
+        }else if(a < b && b > c){
+            System.out.println("Second Number is Largest");
+        }else {
+            System.out.println("Third Number is Largest");
+        }
+    }
+}
