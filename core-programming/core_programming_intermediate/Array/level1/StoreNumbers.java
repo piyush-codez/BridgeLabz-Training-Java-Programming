39354f89e50c78b287f928aef6aa710e06252b16
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-09 12:17:19 +0530

    Program for storing number and calculating sum

---
+++ core-programming/core_programming_intermediate/Array/level1/StoreNumbers.java
@@ -0,0 +1,33 @@
+package core_programming_intermediate.Array.level1;
+
+import java.util.Scanner;
+
+public class StoreNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] numbers = new double[10];
+        double total = 0;
+        int index = 0;
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            double num = sc.nextDouble();
+
+            if (num <= 0 || index == 10) {
+                break;
+            }
+
+            numbers[index] = num;
+            index++;
+        }
+
+        System.out.println("Stored Numbers:");
+        for (int i = 0; i < index; i++) {
+            System.out.println(numbers[i]);
+            total += numbers[i];
+        }
+
+        System.out.println("Total Sum = " + total);
+    }
+}
