a6f5332b94824a64d27bdaafd9b4974b5ec0829a
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-15 10:48:42 +0530

    Program to Simulate Student Report

---
+++ object-oriented-programming/OOP_basics/class_and_object_problems/Student.java
@@ -0,0 +1,43 @@
+package OOP_basics.class_and_object_problems;
+
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
+
+
+    Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+
+
+    char calculateGrade() {
+        if (marks >= 90) {
+            return 'A';
+        } else if (marks >= 75) {
+            return 'B';
+        } else if (marks >= 60) {
+            return 'C';
+        } else if (marks >= 40) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+
+    void displayStudentDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Piyush", 101, 88);
+
+        student1.displayStudentDetails();
+    }
+}
