291092acf9fe419ce21740ca2545670084714129
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-20 16:21:46 +0530

    Add linear search implementation

---
+++ data-structures-algorithms/algorithms/Searching/LinearSearch.java
@@ -0,0 +1,23 @@
+package algorithms.Searching;
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 30;
+
+        int result = linearSearch(arr, target);
+
+        System.out.println("Element Found at Index: " + result);
+    }
+}
