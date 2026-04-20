871f103561d9c5daad52a89b16072c24aa7ce1e7
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-20 15:23:14 +0530

    Add permutation in string sliding window solution

---
+++ data-structures/Sliding_Window/PermutationInString.java
@@ -0,0 +1,51 @@
+package Sliding_Window;
+
+public class PermutationInString {
+
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Count = new int[26];
+        int[] windowCount = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            windowCount[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(s1Count, windowCount)) {
+            return true;
+        }
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+            windowCount[s2.charAt(i) - 'a']++;
+            windowCount[s2.charAt(i - s1.length()) - 'a']--;
+
+            if (matches(s1Count, windowCount)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean matches(int[] arr1, int[] arr2) {
+        for (int i = 0; i < 26; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+
+        boolean result = checkInclusion(s1, s2);
+
+        System.out.println("Permutation Exists: " + result);
+    }
+}
