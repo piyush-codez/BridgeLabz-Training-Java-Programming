ada4928a58b144d4a191124af8951581f5e954eb
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-20 15:54:11 +0530

    Add valid anagram solution using frequency array

---
+++ data-structures/Hashing/ValidAnagram.java
@@ -0,0 +1,34 @@
+package Hashing;
+
+public class ValidAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] frequency = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            frequency[s.charAt(i) - 'a']++;
+            frequency[t.charAt(i) - 'a']--;
+        }
+
+        for (int count : frequency) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        boolean result = isAnagram(s, t);
+
+        System.out.println("Are Anagrams: " + result);
+    }
+}
