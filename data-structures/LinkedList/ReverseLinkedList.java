cbcc75c8e36b00f2799994bb47e8f7f9bc91726c
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-20 11:19:43 +0530

    Add reverse linked list solution using iterative approach

---
+++ data-structures/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,50 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.print("\nReversed List: ");
+        printList(head);
+    }
+}
