369b09e98f39da85cfbc711c95e1c54273089dc2
Piyush Rathor <p.rathor2811@gmail.com>
2026-04-22 16:22:28 +0530

    Program for sorting colors

---
+++ data-structures-algorithms/Review/SortColors.java
@@ -0,0 +1,43 @@
+package Review;
+
+import java.util.Arrays;
+
+class SortColors {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SortColors obj = new SortColors();
+
+        int[] nums1 = {2, 0, 2, 1, 1, 0};
+        int[] nums2 = {2, 0, 1};
+
+        obj.sortColors(nums1);
+        obj.sortColors(nums2);
+
+        System.out.println(Arrays.toString(nums1)); // [0, 0, 1, 1, 2, 2]
+        System.out.println(Arrays.toString(nums2)); // [0, 1, 2]
+    }
+}
